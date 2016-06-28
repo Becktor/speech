250297bae926447dafa686944de0d901313e0322
Jonathan Becktor <Jobe@localhost.localdomain>
2016-06-28 15:25:03 +0900

    bugfix

--- src/DNN.py
+++ src/DNN.py
@@ -104,6 +104,6 @@ if __name__ == '__main__':
                     x: batch[0], y_: batch[1], keep_prob: 1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
+    print('testing now')
     print("test accuracy %g" % accuracy.eval(feed_dict={
                 x: test.spectrograms, y_: test.labels, keep_prob: 1.0}))
