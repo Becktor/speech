69b435b32ac48571c51e47319a68d19fa3cc2a66
Jonathan Becktor <JonathanBecktor@gmail.com>
2016-07-13 15:21:55 +0900

    ff

--- src/DNN-ff.py
+++ src/DNN-ff.py
@@ -28,12 +28,16 @@ test = batch.Batch(testSet)
 # Parameters
 learning_rate = 0.001
 training_epochs = 15
-batch_size = 100
+batch_size = 128
 display_step = 1
 
 # Network Parameters
 n_hidden_1 = 256 # 1st layer number of features
 n_hidden_2 = 256 # 2nd layer number of features
+n_hidden_3 = 256 # 3rd layer number of features
+n_hidden_4 = 256 # 4th layer number of features
+n_hidden_5 = 256 # 5th layer number of features
+
 n_input = 4018   # Dataset data input (img shape: 98*41)
 n_classes = 6    # Dataset total classes (6 emotions)
 
@@ -50,19 +54,35 @@ def multilayer_perceptron(x, weights, biases):
     # Hidden layer with RELU activation
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
     layer_2 = tf.nn.relu(layer_2)
+    # Hidden layer with RELU activation
+    layer_3 = tf.add(tf.matmul(layer_2, weights['h2']), biases['b2'])
+    layer_3 = tf.nn.relu(layer_3)
+    # Hidden layer with RELU activation
+    layer_4 = tf.add(tf.matmul(layer_3, weights['h2']), biases['b2'])
+    layer_4 = tf.nn.relu(layer_4)
+    # Hidden layer with RELU activation
+    layer_5 = tf.add(tf.matmul(layer_4, weights['h2']), biases['b2'])
+    layer_5 = tf.nn.relu(layer_5)
+
     # Output layer with linear activation
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    out_layer = tf.matmul(layer_5, weights['out']) + biases['out']
     return out_layer
 
 # Store layers weight & bias
 weights = {
     'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
     'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
+    'h4': tf.Variable(tf.random_normal([n_hidden_3, n_hidden_4])),
+    'h5': tf.Variable(tf.random_normal([n_hidden_4, n_hidden_5])),
+    'out': tf.Variable(tf.random_normal([n_hidden_5, n_classes]))
 }
 biases = {
     'b1': tf.Variable(tf.random_normal([n_hidden_1])),
     'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+    'b4': tf.Variable(tf.random_normal([n_hidden_4])),
+    'b5': tf.Variable(tf.random_normal([n_hidden_5])),
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
@@ -83,10 +103,10 @@ with tf.Session() as sess:
     # Training cycle
     for epoch in range(training_epochs):
         avg_cost = 0.
-        total_batch = int(mnist.train.num_examples/batch_size)
+        total_batch = int(train.num_examples/batch_size)
         # Loop over all batches
         for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            batch_x, batch_y = train.next_batch(batch_size)
             # Run optimization op (backprop) and cost op (to get loss value)
             _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
                                                           y: batch_y})
@@ -102,4 +122,4 @@ with tf.Session() as sess:
     correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-    print "Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels})
+    print "Accuracy:", accuracy.eval({x: test.images, y: test.labels})
