f572a58997bf32a3fa8748d498ba70b67e1e5a01
Jonathan Becktor <Jobe@localhost.localdomain>
2016-07-13 13:08:35 +0900

    Change back to adamOptimizer and some changes to disp" "

--- src/disp.py
+++ src/disp.py
@@ -3,7 +3,19 @@ import scipy, sys
 import scipy.io.wavfile as sc
 from scipy.fftpack import dct
 import matplotlib.pyplot as plt
+import math
 
-output_name = "tst.mat"
-matdata2 = scipy.io.loadmat(output_name)['out']
-print matdata2.shape
+output_name = "TrainBatch2.mat"
+matdata2 = scipy.io.loadmat(output_name)['arr']
+maxi = 0.
+mini = float("inf")
+for vec in matdata2:
+    tmp_max=max(vec)
+    if tmp_max>maxi:
+        maxi=tmp_max
+    tmp_min=min(vec)
+    if tmp_min<=min:
+        mini=tmp_min
+
+print maxi
+print mini
